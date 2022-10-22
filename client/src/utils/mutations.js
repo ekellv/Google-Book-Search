181d13640ec3efa70ffae9ff3bfbb7fa97bd108a
Erin V <erinkvoelker@gmail.com>
2022-10-21 21:06:02 -0400

    attempting error correction

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,6 +1,6 @@
 import { gql } from '@apollo/client';
 
-// mutation for logged in user
+// user login mutation
 export const LOGIN_USER = gql`
     mutation loginUser($email: String!, $password: String!) {
         login(email: $email, password: $password) {
@@ -23,7 +23,7 @@ export const LOGIN_USER = gql`
    }
 `;
 
-// mutation to add user 
+// add user mutation
 export const ADD_USER = gql`
     mutation addUser($username: String!, $email: String!, $password: String!) {
         addUser(username: $username, email: $email, password: $password) {
@@ -46,9 +46,9 @@ export const ADD_USER = gql`
     }
 `;
 
-// mutation to save books
+// save books mutation
 export const SAVE_BOOK = gql`
-    mutation saveBook($input: input!) {
+    mutation saveBook($input: bookInput!) {
         saveBook(input: $input) {
             _id
             username
@@ -65,7 +65,7 @@ export const SAVE_BOOK = gql`
     }
 `;
 
-// mutation to remove books
+// remove books mutation
 export const REMOVE_BOOK = gql`
     mutation removeBook($bookId: String!) {
         removeBook(bookId: $bookId) {
